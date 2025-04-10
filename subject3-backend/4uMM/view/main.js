708a66dcf31f163a7111fde37f058b29f923789e
ned1010 <nidup1998@gmail.com>
2025-04-10 17:11:17 +0800

    Lesson 45

--- subject3-backend/4uMM/view/main.js
+++ subject3-backend/4uMM/view/main.js
@@ -55,6 +55,10 @@ function getData() {
               ${data.data[i].content}
             </p>
           </div>
+          <div class='buttons'>
+          <button onclick="handleDeleteForum('${data.data[i].id}')">Delete</button>
+          <button onclick="handleEditForum('${data.data[i].id}','${data.data[i].title}', '${data.data[i].content}')">Edit </button>
+          </div>
         </li>`
       }
 
@@ -63,4 +67,93 @@ function getData() {
       console.log(error);
     }
   });
+}
+
+
+//create a forum
+async function handleCreateForum(event){
+  event.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const content = document.getElementById('content').value;
+  console.log(title, content);
+
+  const newForum = {
+    title: title,
+    content: content
+  }
+
+  //URL to makae the post request
+  const url = 'http://localhost:3000/new-post';
+  try {
+    const options = {
+      'method': "POST",
+      "headers": {
+        'Content-Type': 'application/json'
+      },
+      'body': JSON.stringify(newForum)
+    }
+
+    const response = await fetch(url, options);
+    if (response.status === 200){
+      const {data, message} = await response.json();
+      alert(message);
+      window.location.href = 'index.html'
+    } else{
+      const {message} = await response.json();
+      alert(message)
+    }
+  } catch (error) {
+    console.log(`Error creating a forum from frontend ${error}`)
+  }
+
+
+}
+
+//delete a forum
+async function handleDeleteForum(id){
+  console.log(id);
+  
+  const url = `http://localhost:3000/delete-post/${id}`
+  try {
+    const options = {
+      'method': "DELETE",
+      'headers': {
+        'Content-Type': 'application/json'
+      }
+    }
+    const response = await fetch(url, options);
+    if (response.status === 200){
+      const {data, message} = await response.json();
+      alert(message);
+      window.location.href = 'index.html'
+    } else{
+      const {message} = await response.json();
+      alert(message)
+    }
+  } catch (error) {
+    console.log(`Error deleting a forum from frontend ${error}`)
+  }
+}
+
+//edit a forum
+async function handleEditForum(id, title, content){
+  // console.log(id);
+  const updateContainer = document.getElementById('update-container');
+  updateContainer.classList.remove('hide');
+
+  const titleExtracted = document.getElementById('title');
+  titleExtracted.value = title
+
+  const contentExtracted = document.getElementById('content');
+  contentExtracted.value = content
+
+  // console.log(id, title, content)
+
+  try {
+    
+  } catch (error) {
+    console.log(`Error editing a forum from frontend ${error}`)
+  }
+
 }
